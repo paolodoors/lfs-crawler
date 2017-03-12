07a0efeb4fab1381464c3bb883a2996d3bbb4fa9
Andrew Kanner <andrew.kanner@gmail.com>
2017-03-12 11:01:22 +0300

    run.sh: added cleaning + creating folder structure

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -ex
 
-CHROME_DOWNLOAD_PATH="~/Downloads/"
+CHROME_DOWNLOAD_PATH=~/Downloads/
 
 if [ "$1" == "-r" ]; then
 	rm ${CHROME_DOWNLOAD_PATH}/LF*.html 2> /dev/null
 else
 	echo -n "Copying html ... "
-	mv ${CHROME_DOWNLOAD_PATH}/LF*.html html/ori 2> /dev/null
-	rm html/clean/* 2> /dev/null
-	rm -f md/* 2> /dev/null
-	cp html/ori/* html/clean/
+	rm -rf ./html/ ./md/
+	mkdir -p ./html/{orig,clean}
+	mv ${CHROME_DOWNLOAD_PATH}/LF*.html html/orig #2> /dev/null
+#	rm html/clean/* 2> /dev/null
+#	rm -f md/* 2> /dev/null
+	cp html/orig/* html/clean/
 	rename 's/_popup.html$/_popup (0).html/' html/clean/*.html
 	echo "ok"
 fi
