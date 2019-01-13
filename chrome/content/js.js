633820c6d1a9b3b4abddd5e6ed671870da032b57
Paolo Stancato <paolo.stancato@fluxitsoft.com>
2019-01-13 16:04:46 -0300

    Cambios en la extension de Chrome para desacargar LFS258

--- chrome/content/js.js
+++ chrome/content/js.js
@@ -24,15 +24,22 @@
 				}
 				if (toc==null || toc.selectedOptions==null || toc.selectedOptions.length==0) return;
 				var title=toc.selectedOptions[0].label.replace(/^\s+|\s+$/g,"");
+//				window.alert(title);
 				var ch=null;
 				var i,a;
 				for (i=(toc.selectedOptions[0].index-1);(i>=0 && ch==null);i--) {
 					a=toc.options[i].label.replace(/^\s+|\s+$/g,"")
-					if (/^(Cap|ch)\.\s+(\d+)\..+$/i.test(a)) {
-						ch=a.replace(/^(Cap|ch)\.\s+(\d+)\..+$/ig,"$2");
+					if (/^(Cap|Chap)\.\s+(\d+)\..+$/i.test(a)) {
+						ch=a.replace(/^(Cap|Chap)\.\s+(\d+)\..+$/ig,"$2");
 					}
 				}
-				if (ch==null) return;
+//				window.alert(toc.selectedOptions[0]);
+//				window.alert(ch);
+				if (ch==null) {
+				    h1 = document.getElementsByTagName('h1');
+				    r = h1[0].textContent.split('.');
+				    ch = r[0];
+				}
 				title=((Number(ch)+100)+"").substring(1)+"."+((Number(toc.selectedOptions[0].index-i+1)+100)+"").substring(1)+"_"+title.replace(/\s+/g,"_");
 				if (popup==1) {
 					tt=window.parent.document.getElementsByClassName("DLG_Triv_titleText")[0];
@@ -48,7 +55,7 @@
 					img.src=img.src;
 				}
 
-				var f="LFS201_"+title+".html";
+				var f="LFS258_"+title+".html";
 
 				var textToWrite = document.documentElement.outerHTML;
 				var textFileAsBlob = new Blob([textToWrite], {type:'text/html'});
@@ -63,8 +70,13 @@
 				/** this one is for Spanish version (not confirmed) */
 				//var a=document.getElementsByName("45297anc");
 				/** this one is for English version (confirmed, March 2017) */
-				var a=document.getElementsByName("43169anc");
-				if (a.length==1) a[0].click();				
+				var a=document.getElementsByName("13887anc");
+				if (a.length==1) {
+					a[0].click();
+				} else {
+					var a=document.getElementsByName("13924anc");
+					if (a.length==1) a[0].click();
+				}
 			},
 			run:function(doc){
 				setTimeout(this.save,2000);
